5eb103b7843644f2a6c49237453d0143f57bdcec
Jose Fernandez <Josefernandez280@gmail.com>
2018-07-24 10:52:05 -0400

    fixed pages

--- student_models.py
+++ student_models.py
@@ -4,10 +4,12 @@ class Student(ndb.Model):
     firstName = ndb.StringProperty(required=True)
     lastName = ndb.StringProperty(required=True)
     email = ndb.StringProperty(required=True)
+    password = ndb.StringProperty(required=True)
     college = ndb.StringProperty(required=True)
     major = ndb.StringProperty(required=True)
-    zipCode = ndb.StringProperty(required=True)
+    ##zipCode = ndb.StringProperty(required=True)
     description = ndb.StringProperty(required=True)
+    connections = ndb.StringProperty(repeated=True,required=False)
 
 class areasOfInterest(ndb.Model):
     interests = ndb.KeyProperty(Student, repeated=True, required = False)
